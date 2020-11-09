65a1a1f7f797f519157c44e3c7da202c5bf08cd2
Daniel Reichhart <reichhartd@MacBook-Pro-von-Daniel.local>
2020-11-09 12:17:43 +0100

    Fixed sidebar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import * as styles from './App.module.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export const App = () => (
+    <div className={styles.container}>
+        <div className={styles.sidenav}>
+            <a href="#about">About</a>
+            <a href="#services">Services</a>
+            <a href="#clients">Clients</a>
+            <a href="#contact">Contact</a>
+        </div>
+        <div className={styles.main}>
+            <h2>Sidebar</h2>
+            <p>This sidebar is of full height (100%) and always shown.</p>
+            <p>Scroll down the page to see the result.</p>
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((number) => (
+                <p key={number}>
+                    {`${number}. Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset
+                    concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur
+                    eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae
+                    voluptatibus.`}
+                </p>
+            ))}
+        </div>
     </div>
-  );
-}
-
-export default App;
+);
