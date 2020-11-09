26094f3a827aaa62f21714edb00d853da392a270
Daniel Reichhart <reichhartd@MacBook-Pro-von-Daniel.local>
2020-11-09 15:24:40 +0100

    Solution 1

--- src/App.js
+++ src/App.js
@@ -3,16 +3,46 @@ import * as styles from './App.module.css';
 export const App = () => (
     <div className={styles.container}>
         <div className={styles.sidenav}>
-            <a href="#about">About</a>
-            <a href="#services">Services</a>
-            <a href="#clients">Clients</a>
-            <a href="#contact">Contact</a>
+            {[
+                'about',
+                'services',
+                'clients',
+                'contact',
+                'about',
+                'services',
+                'clients',
+                'contact',
+                'about',
+                'services',
+                'clients',
+                'contact',
+                'about',
+                'services',
+                'clients',
+                'contact',
+                'about',
+                'services',
+                'clients',
+                'contact',
+                'about',
+                'services',
+                'clients',
+                'contact',
+                'about',
+                'services',
+                'clients',
+                'contact',
+            ].map((link) => (
+                <a href={`#${link}`} key={link}>
+                    {link.toUpperCase()}
+                </a>
+            ))}
         </div>
         <div className={styles.main}>
             <h2>Sidebar</h2>
             <p>This sidebar is of full height (100%) and always shown.</p>
             <p>Scroll down the page to see the result.</p>
-            {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((number) => (
+            {[...Array(10).keys()].map((number) => (
                 <p key={number}>
                     {`${number}. Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset
                     concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur
